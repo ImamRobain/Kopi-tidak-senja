6b81fed364272208dc082e676422f48f871074b9
Imam Robain <imamrobain16@gmail.com>
2023-05-29 23:44:02 +0700

    Menambahkan Search Box, Produk, Shopping Cart dan Deteil Prodok

--- js/script.js
+++ js/script.js
@@ -1,15 +1,68 @@
-// Toggle class active
+// Toggle class active Hamburger Menu
 const navbarNav = document.querySelector(".navbar-nav");
 // Hamburger di klik
 document.querySelector("#hamburger-menu").onclick = () => {
   navbarNav.classList.toggle("active");
 };
 
-// Klik diluar sidebar untuk menghilangkan nav
+// Toggle class active Search Form
+const searchForm = document.querySelector(".search-form");
+const searchBox = document.querySelector("#search-box");
+
+document.querySelector("#search-button").onclick = (e) => {
+  searchForm.classList.toggle("active");
+  searchBox.focus();
+  e.preventDefault();
+};
+// Toggle class active Shopping Cart
+const shoppingButton = document.querySelector(".shopping-cart");
+document.querySelector("#shopping-cart-button").onclick = (e) => {
+  shoppingButton.classList.toggle("active");
+  shoppingButton.focus();
+};
+// Klik diluar Elemen
 const hamburger = document.querySelector("#hamburger-menu");
+const searchButton = document.querySelector("#search-button");
+const shoppingCart = document.querySelector("#shopping-cart-button");
 
 document.addEventListener("click", function (e) {
   if (!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
     navbarNav.classList.remove("active");
   }
+
+  document.addEventListener("click", function (e) {
+    if (!searchButton.contains(e.target) && !searchForm.contains(e.target)) {
+      searchForm.classList.remove("active");
+    }
+  });
+  document.addEventListener("click", function (e) {
+    if (
+      !shoppingCart.contains(e.target) &&
+      !shoppingButton.contains(e.target)
+    ) {
+      shoppingButton.classList.remove("active");
+    }
+  });
 });
+// Modal Box
+const itemDetailModal = document.querySelector("#item-detail-modal");
+const itemDetailButtons = document.querySelectorAll(".item-detail-button");
+
+itemDetailButtons.forEach((btn) => {
+  btn.onclick = (e) => {
+    itemDetailModal.style.display = "flex";
+    e.preventDefault();
+  };
+});
+
+// Klik Tombol Close Modal
+document.querySelector(".modal .close-icon").onclick = (e) => {
+  itemDetailModal.style.display = "none";
+  e.preventDefault();
+};
+// Klik Diluar Modal
+window.onclick = (e) => {
+  if (e.target === itemDetailModal) {
+    itemDetailModal.style.display = "none";
+  }
+};
